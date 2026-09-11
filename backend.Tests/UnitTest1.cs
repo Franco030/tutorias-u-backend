ec63573840c4502b2b3ee1e710570b4de9b5e506
Franco030 <edwinfranco3104@hotmail.com>
2026-09-11 00:16:29 -0600

    chore: Limpiar el codigo de archivos que ya no se usaban y un unit test que va a fallar

---
+++ backend.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace backend.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Sumar_DosNumeros()
+        {
+            Assert.Equal(10, 2);
+        }
+    }
+}
