8a260bbc6379cfa03c3956ba10591d711100452b
Edwin Jaziel Franco Arellano <edwinfranco3104@hotmail.com>
2026-09-16 14:58:02 -0600

    hotfix: Archivos innecesarios

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using backend.Models;
@@ -16,18 +16,23 @@ public partial class AppDbContext : DbContext
     {
     }
 
+    public virtual DbSet<EstadosAprobacion> EstadosAprobacion { get; set; }
+
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
-        }
-    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<EstadosAprobacion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__EstadosA__3214EC07A376A07F");
+
+            entity.ToTable("EstadosAprobacion");
+
+            entity.Property(e => e.Estado).HasMaxLength(50);
+        });
+
         modelBuilder.Entity<Usuario>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Usuarios__3214EC077101C9AC");
@@ -36,13 +41,19 @@ public partial class AppDbContext : DbContext
 
             entity.Property(e => e.AuthProvider).HasMaxLength(50);
             entity.Property(e => e.Email).HasMaxLength(150);
+            entity.Property(e => e.EstadoAprobacionId).HasDefaultValue(1, "DF__Usuarios__EstadoAprobacionId");
             entity.Property(e => e.FechaRegistro).HasDefaultValueSql("(getdate())");
             entity.Property(e => e.FotoUrl).HasMaxLength(500);
             entity.Property(e => e.Nombre).HasMaxLength(100);
             entity.Property(e => e.ProviderId).HasMaxLength(100);
             entity.Property(e => e.Rol)
                 .HasMaxLength(50)
-                .HasDefaultValue("Estudiante");
+                .HasDefaultValue("Nuevo", "DF__Usuarios__Rol");
+
+            entity.HasOne(d => d.EstadoAprobacion).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.EstadoAprobacionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Usuarios__EstadoAprobacionId");
         });
 
         OnModelCreatingPartial(modelBuilder);
